0787a5c245f5d7fa2e9022531f490d1870ef6c2f
shaffiq <shaffiqr@mail.ubc.ca>
2013-02-14 15:41:06 -0800

    Changed param name to parentslug - thx Michael
    SSC links added target to open in new window
    Changed Cache flags to smaller calendar icon - gray = from server light
    gray = from transients - less confusion for cached pages

--- ubccourses-shortcode.php
+++ ubccourses-shortcode.php
@@ -26,12 +26,12 @@ class UBCCourses {
 		add_shortcode('ubccourses', array($this, 'shortcode'));
 	}
 	
-        private function getDetailsBtn($pageName,$parentPermalink){
+        private function getDetailsBtn($pageName,$parentslug){
            $btnHTML = '';
            $page = get_page_by_title($pageName);
            if($page){
               $pageLink = get_page_uri($page->ID);
-              if (dirname($pageLink) == $parentPermalink){
+              if (dirname($pageLink) == $parentslug){
                 $btnHTML = '<a href="'.$pageLink.'" role="button" style="margin-left:5px;" class="btn btn-info btn-mini">Details</a>';
               }
            }
@@ -39,17 +39,17 @@ class UBCCourses {
         }
 
 	
-	private function getList($department, $course, $pills, $pillcount, $tabs, $tabcount, $parentPermalink){
+	private function getList($department, $course, $pills, $pillcount, $tabs, $tabcount, $parentslug){
 		include_once 'ubcCalendarAPI.php';
 		$ubccalendarAPI = new ubcCalendarAPI($department, $course, false);
                 $xml = simplexml_load_string($ubccalendarAPI->XMLData);
-                if(!$ubccalendarAPI->fromTransient)
-                   $fserver_label = '<button class="btn btn-mini btn-danger status" type="button">from Server</button>';
-                else 
-                   $fserver_label = '<button class="btn btn-mini btn-success status" type="button">from Transients</button>';
+                if($ubccalendarAPI->fromTransient)
+                   $fserver_label = '<i style="margin-left:4px;color:#dfdfdf;" class="icon-calendar"></i>';
+                else
+                   $fserver_label = '<i style="margin-left:4px;color:gray;" class="icon-calendar"></i>';
                 $count = 0;
                 foreach ($xml->course as $courses) { 
-                   $detailsbtn = $this->getDetailsBtn($department.$courses['key'],$parentPermalink);
+                   $detailsbtn = $this->getDetailsBtn($department.$courses['key'],$parentslug);
                    $params = "'".$department."', '".$courses['key']."' ";  
                    $section = '<a onclick="getSectionData('.$params.');" href="#myModal" role="button" class="btn btn-mini modalbox" data-toggle="modal">Sections</a>';
                    if (empty($course)&&($pills)||empty($course)&&($tabs)){
@@ -101,16 +101,16 @@ class UBCCourses {
 		include_once 'ubcCalendarAPI.php';
 		$ubccalendarAPI = new ubcCalendarAPI($department, $course, true);
                 $xml = simplexml_load_string($ubccalendarAPI->XMLData);
-                if(!$ubccalendarAPI->fromTransient)
-                    $fserver_label = '<button class="btn btn-mini btn-danger status" type="button">from Server</button>';
-                else 
-                    $fserver_label = '<button class="btn btn-mini btn-success status" type="button">from Transients</button>';
+                if($ubccalendarAPI->fromTransient)
+                   $fserver_label = '<i style="margin-left:4px;color:#dfdfdf;" class="icon-calendar"></i>';
+                else
+                   $fserver_label = '<i style="margin-left:4px;color:gray;" class="icon-calendar"></i>';
                 $count = 0; 
                 $output = '<table id="ubccsections"><td><strong>Sec</strong></td><td><strong>Activity</strong></td><td><strong>Term</strong></td><td><strong>Day</strong></td><td><strong>Bld</strong></td><td><strong>Instructor</strong></td>'; 
                 foreach ($xml->section as $sections) {           
                    $ssc_link = "https://courses.students.ubc.ca/cs/main?"."pname=subjarea&tname=subjareas&req=5&dept=".$department."&course=".$course."&section=".$sections['key']."&sessyr=".$ubccalendarAPI->currentYear."&sesscd=".$ubccalendarAPI->currentSession;
                    $inst_link = "https://courses.students.ubc.ca/cs/main?pname=inst&ubcid=".$sections->instructors->instructor['ubcid'];
-                   $output .= '<tr><td><a href="'.$ssc_link.'">'.$sections['key'].'</a></td><td>'.$sections['activity'].'</td>';
+                   $output .= '<tr><td><a target="_blank" href="'.$ssc_link.'">'.$sections['key'].'</a></td><td>'.$sections['activity'].'</td>';
                    $meetings = $sections->teachingunits->teachingunit->meetings->meeting;
                    $term =array();$day =array();$bld =array();
                    foreach ($meetings as $meeting){
@@ -122,7 +122,7 @@ class UBCCourses {
 	                 array_push($bld,trim($meeting['buildingcd']));
                    }
                    $output .= '<td>'.implode(" ",$term).'</td><td>'.implode(" ",$day).'</td><td>'.implode(" ",$bld).'</td>';
-                   $output .= '<td><a href="'.$inst_link.'">'.$sections->instructors->instructor['name'].'</a></td></tr>';
+                   $output .= '<td><a target="_blank" href="'.$inst_link.'">'.$sections->instructors->instructor['name'].'</a></td></tr>';
                    $count ++;
                  }
                  $output .= '</table>';
@@ -148,12 +148,12 @@ class UBCCourses {
              "pillcount" => 4,
              "tabs" => false,
              "tabcount" => 4,
-             "parentpermalink" => ''
+             "parentslug" => ''
              ), $atts));
 		
              //Get Ajax url and setup js vars
              $ajaxurl = admin_url('admin-ajax.php' );
-             return '<script> var ajaxurl = "'.$ajaxurl.'"; </script>'.$this->getList( $department, $course, $pills, $pillcount, $tabs, $tabcount, $parentpermalink);
+             return '<script> var ajaxurl = "'.$ajaxurl.'"; </script>'.$this->getList( $department, $course, $pills, $pillcount, $tabs, $tabcount, $parentslug);
 	}
 }
 
